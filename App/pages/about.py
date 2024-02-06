7a02875176568a3c53975231c86fb831a35d5ab1
yGrouzelle <yan.grouzelle@edu.ece.fr>
2024-02-06 16:29:29 +0100

    f

--- App/pages/about.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.title("Notre projet est de simplifier votre découverte du vin")
-st.write("mywine est une application basée sur un algorithme de reconnaissance de formes et caratères. Permettant d'apporter de manière instantanné à partir d'un scan de l'étiquettes d'une bouteille de nombreuse informations relative à cette bouteille. ")
-
