70acf55235b1d5297ce8cf1a6a1cc3f4519f3b8f
yGrouzelle <yan.grouzelle@edu.ece.fr>
2024-02-07 18:21:46 +0100

    fonction import + css

--- app/main.py
+++ app/main.py
@@ -12,15 +12,25 @@ from fonction_bouton import *
 from fonction_bdd import *
 
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] {{
+background-image: url("https://www.neozone.org/blog/wp-content/uploads/2021/03/vin-002-780x470.jpg");
+background-size: cover;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # titre de la page
-st.title("Découvre ce que tu bois !")
+#st.title("Découvre ce que tu bois !")
 
 #varialbes utiliser pour appel du model
 model_path = "./best.pt"
 model = YOLO(model_path)
 if "bouton" not in st.session_state:
     st.session_state.bouton = False
-st.button("Open/Close Camera", on_click=active_cam)
+#st.button("Open/Close Camera", on_click=active_cam)
 
 # Vérifier si la caméra est ouverte avant de capturer une photo
 if st.session_state.bouton:
@@ -30,8 +40,28 @@ if st.session_state.bouton:
         img=Image.open(picture)
         img_array=np.array(img)
         models(img_array, model)
-    
+#fonction utilisé pour verifier la conexion a internet
 
+def check_internet():
+    try:
+        requests.get('http://www.google.com', timeout=1)
+        return True
+    except requests.ConnectionError:
+        return False
+
+if check_internet():
+    st.write("Vous êtes connecté à Internet.")
+else:
+    st.write("Vous n'êtes pas connecté à Internet.")
+
+# titre de la page
+st.markdown(
+    f"""
+    <h1 style='text-align: center;'>Découvre ce que tu bois 🍷 !</h1>
+     <style>
+    """,
+    unsafe_allow_html=True
+)  
 
 #creation de bouton pour filtrer par rapport au region
 st.button('selectionner un filtre',on_click=active_filre)
@@ -46,31 +76,10 @@ if st.session_state.mod:
    index=None,
    placeholder="Selectionner une region ...",
 )
-#creation d'une variable pour stocker l'option
-
-# Bouton pour stocker la région sélectionnée dans la variable de session
-if st.button("Importer la région :"):
-    st.session_state.region_selectionnee = option
-    st.write(f"Région sélectionnée pour l'importation : {st.session_state.region_selectionnee}")
     
-if "bouton_postgre" not in st.session_state:
-    st.session_state.bouton_postgre = False
-def active_affiche_data():
-    st.session_state.bouton_postgre= not st.session_state.bouton_postgre 
-
-st.button('Afficher les données depuis postgres',on_click=active_affiche_data())
-
-
-if st.session_state.bouton_postgre:
-    data = run_query("SELECT * FROM vins")
-    for row in data:
-        st.write(row)
-
+#creation d'une variable pour stocker l'option
 def truncate_table(table_name):
     run_query(f"TRUNCATE TABLE {table_name} CASCADE;")
-
-# Exportation des données supabase vers postgreSQL
-        
 def extract_from_supabase(api_url, headers, nom_region):
     params = {'region': f'eq.{nom_region}'}
     response = requests.get(api_url, headers=headers, params=params)
@@ -86,9 +95,9 @@ def import_to_postgres(local_db_connection_string, table_name, data):
 # Si les données sont sous forme de JSON et doivent être converties en CSV
     csv_data = StringIO()
     writer = csv.writer(csv_data)
-    writer.writerow(['id', 'Nom', 'Date','Description', 'Note', 'cepage', 'region'])  # Entêtes de colonnes
+    writer.writerow(['id', 'etiquette', 'Date','Description', 'Note', 'cepage', 'region','Nom','Conservation'])  # Entêtes de colonnes
     for item in data:
-        writer.writerow([item['id'], item['Nom'], item['Date'], item ['Description'],  item['Note'], item['cepage'], item['region']])
+        writer.writerow([item['id'], item['etiquette'], item['Date'], item ['Description'],  item['Note'], item['cepage'], item['region'], item['Nom'],item['Conservation']])
 
     # Se déplacer au début du StringIO pour lire son contenu
     csv_data.seek(0)
@@ -98,8 +107,9 @@ def import_to_postgres(local_db_connection_string, table_name, data):
     conn.commit()
     cur.close()
     conn.close()
-
-if st.button('Importer sous-table region depuis Supabase') and st.session_state.region_selectionnee:
+# Bouton pour stocker la région sélectionnée dans la variable de session
+if st.button("valider"):
+    st.session_state.region_selectionnee = option
     try:
          # Effacer les données de la table "vins" avant l'importation
         truncate_table("vins")
@@ -110,6 +120,7 @@ if st.button('Importer sous-table region depuis Supabase') and st.session_state.
             'Content-Type': 'application/json',
             'apikey' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbnlzamRxYWV6bWpzanZzeW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU0Mjc1NTEsImV4cCI6MjAyMTAwMzU1MX0.wjQT5SHkmJIT0aKOZNHwx5ciAqL6PrkemYladC5T1l0'
         }
+       
         nom_region = st.session_state.region_selectionnee
 
         data = extract_from_supabase(supabase_api_url, supabase_headers, nom_region)
@@ -125,18 +136,28 @@ if st.button('Importer sous-table region depuis Supabase') and st.session_state.
         st.success('Importation réussie !')
     except Exception as e:
         st.error(f'Une erreur est survenue : {e}')
+   
+    
+if "bouton_postgre" not in st.session_state:
+    st.session_state.bouton_postgre = False
+def active_affiche_data():
+    st.session_state.bouton_postgre= not st.session_state.bouton_postgre 
 
-#fonction utilisé pour verifier la conexion a internet
+st.button('Afficher les données depuis postgres',on_click=active_affiche_data())
 
-def check_internet():
-    try:
-        requests.get('http://www.google.com', timeout=1)
-        return True
-    except requests.ConnectionError:
-        return False
 
-if check_internet():
-    st.write("Vous êtes connecté à Internet.")
-else:
-    st.write("Vous n'êtes pas connecté à Internet.")
+if st.session_state.bouton_postgre:
+    data = run_query("SELECT * FROM vins")
+    for row in data:
+        st.write(row)
+
+
+
+# Exportation des données supabase vers postgreSQL
+        
+
+st.button("📷 Capture une Photo", on_click=active_cam, key="my_button", help="Capture une photo")
+
+
+
 
